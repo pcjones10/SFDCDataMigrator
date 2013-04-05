3ce8f206be424a35c3cf459f31b08463e8777a98
Philip Jones <pcjones10@gmail.com>
2013-04-05 10:56:39 -0700

    Inserting but with duplicate parents.

--- main.php
+++ main.php
@@ -11,73 +11,117 @@ $mySforceConnection = new SforceEnterpriseClient();
 $mySforceConnection->createConnection("enterprise.wsdl.xml");
 $mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
 
-$files = scandir('data/');
 $sObjectIdMap = array();
-foreach($files as $file){
-	$sObjects = array();
-	$fieldArray = array();
-	echo 'Processing file : ' . $file . "\n";
-	$validObj = true;
-	try{
-		$objName = basename($file,'.csv');
-		$describedObj = $mySforceConnection->describeSObject($objName);
-	}catch(Exception $e){
-		$validObj = false;
-	}
-	if($validObj){
-		$isDetail = false;
-		$isRequired = false;
-		
-		foreach($describedObj->fields as $field){
-			if($field->updateable == '1' && $field->type != 'reference'){
-				$fieldArray[$field->name] = true;
-			}else{
-				$fieldArray[$field->name] = false;
+main(false, $mySforceConnection, $sObjectIdMap);
+
+function main($processChildren, $mySforceConnection, &$sObjectIdMap){
+	$files = scandir('data/');
+	foreach($files as $file){
+		$fieldArray = array();
+		echo 'Processing file : ' . $file . "\n";
+		$validObj = true;
+		try{
+			$objName = basename($file,'.csv');
+			$describedObj = $mySforceConnection->describeSObject($objName);
+		}catch(Exception $e){
+			$validObj = false;
+		}
+		if($validObj){
+			$isDetail = false;
+			$isRequired = false;
+			
+			//loop through fields of object and determine if field should be set in object.
+			foreach($describedObj->fields as $field){
+				//if field is updateable and not a lookup then use field
+				if($field->updateable == '1' && $field->type != 'reference'){
+					$fieldArray[$field->name] = true;
+				
+				//if not updateable, is a lookup, is a custom field and processing child
+				//then set special case 
+				}else if($field->updateable != '1' 
+				&& $field->type == 'reference'
+				&& strrpos($field->name,'__c') != false
+				&& $processChildren){
+					echo $field->name . " is detail!!\n";
+					$fieldArray[$field->name] = 'detail';
+				
+				//else do not use field
+				}else{
+					$fieldArray[$field->name] = false;
+					
+				}
+				
+				//if field is lookup, not updateable and custom then set object as detail
+				if(($field->type == 'reference' 
+				&& $field->updateable != '1' 
+				&& strrpos($field->name,'__c') != false)){
+					$isDetail = true;
+				}
 			}
 			
-			if(($field->type == 'reference' && $field->updateable != '1' && strrpos($field->name,'__c') != false)){
-				//echo $objName . ' : ' . $field->label . ', ' . $field->type . ' - ' . $field->updateable;
-				$isDetail = true;
+			insertSObjects($mySforceConnection, $file, $fieldArray, $sObjectIdMap);
+		}
+	}
+	if(!$processChildren){
+		main(true, $mySforceConnection, $sObjectIdMap);
+	}else{
+		createIdHashMap($mySforceConnection, $sObjectIdMap);
+	}
+}
+
+function insertSObjects($mySforceConnection, $file, $fieldArray, &$sObjectIdMap){
+	$csv = new parseCSV();
+	$csv->auto('data/'.$file);
+	$sObjects = array();
+	foreach ($csv->data as $key => $row){
+		$sObject = new stdClass();
+		foreach ($row as $column => $value){
+			echo $file . ' ~!~ ' . $column . ':' . $fieldArray[$column] . ', value: ' . $value . "\n";
+			if($fieldArray[$column] === 'detail' && strlen($value) > 0){
+				echo 'old parent id:' . $value . 
+				' - new parent id:' . $sObjectIdMap[$value] . "\n";
+				$sObject->$column = $sObjectIdMap[$value];
+			}else if($fieldArray[$column] && strlen($value) > 0){
+				$sObject->$column = utf8_encode($value);
 			}
 		}
-		
-		if(!$isDetail){
-			echo 'Parent found : ' . $file . "\n";
-			$csv = new parseCSV();
-			$csv->auto('data/'.$file);
-			foreach ($csv->data as $key => $row){
-				$sObject = new stdClass();
-				foreach ($row as $column => $value){
-					if($fieldArray[$column] && strlen($value) > 0){
-						$sObject->$column = utf8_encode($value);
-					}
-				}
-				print_r($sObject);
-				array_push($sObjects,$sObject);
-				try{
-					//$results = $mySforceConnection->create(array($sObject),$objName);
-					//$sObjectIdMap[$sObject->Id] = $results[0]->Id;
-				}catch(Exception $e){
-				}
-			}
-		}	
+		print_r($sObject);
+		array_push($sObjects,$sObject);
 	}
 	if(count($sObjects) > 0){
+		$objName = basename($file,'.csv');
 		$results = $mySforceConnection->create($sObjects,$objName);
-		echo(count($sObjects) . " Reslults:\n");
 		print_r($results);
-		return;
+		foreach ($csv->data as $key => $row){
+			echo 'old id: ' . $row['Id'] . ' - new id: ' . 
+			$results[$key]->id . ' :: ' . $key . "\n";
+			
+			$sObjectIdMap[$row['Id']] = $results[$key]->id;
+		}
+		print_r($sObjectIdMap);
 	}
 }
-/*
-foreach($files as $file){
-	// read the file
-	$fileContents = file_get_contents($file);
-	// replace the data
-	foreach ($sObjectIdMap as $oldId => $newId){
-		$fileContents = str_replace($oldId, $newId, $fileContents);
+
+function createIdHashMap($mySforceConnection, &$sObjectIdMap){
+	$files = scandir('data/');
+	foreach($files as $file){
+		$validObj = true;
+		try{
+			$objName = basename($file,'.csv');
+			$describedObj = $mySforceConnection->describeSObject($objName);
+		}catch(Exception $e){
+			$validObj = false;
+		}
+		echo $validObj . "\n";
+		if($validObj){
+			echo "writing file...\n";
+			$fileContents = file_get_contents('data/'.$file);
+			foreach ($sObjectIdMap as $oldId => $newId){
+				$fileContents = str_replace($oldId, $newId, $fileContents);
+			}
+			$handle = fopen('data/'.$file,'w');
+			fwrite($handle,$fileContents);
+			fclose($handle);
+		}
 	}
-	// write the file
-	file_put_contents($file, $fileContents);
 }
-print_r($sObjectIdMap);*/
